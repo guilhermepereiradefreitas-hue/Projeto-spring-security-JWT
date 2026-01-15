7ead55df6a5f1a7b734e50a8c403bcac779bf048
Guilherme Pereira <guilhermepereiradefreitas@gmail.com>
2026-01-15 15:10:20 -0300

    Add files via upload

---
+++ src/main/java/Projeto/Projeto_spring_security_JWT/ProjetoSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package Projeto.Projeto_spring_security_JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projeto/Projeto_spring_security_JWT/ProjetoSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.Projeto_spring_security_JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
